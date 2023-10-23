1b1d54714828ebc9ed54eb0a5eb3a3de7852a5f5
HND <hans.doderlein99@gmail.com>
2023-10-23 19:13:40 -0400

    built resume page, fixed CSS

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,3 +1,41 @@
+import "./Resume.css";
+import { HeaderH2, HeaderH3 } from "../../components/Header/Header";
+import { frontEnd, backEnd, sourceControl } from "../../utilities/proficencies";
+
 export function Resume() {
-  return <h2>Resume</h2>;
+  return (
+    <div className="resumePage">
+      <div className="resumeLink">
+        Dowload My Resume &nbsp;
+        <a href="../Files/Resume.docx">Here</a>
+      </div>
+
+      <div className="resumeInfo">
+        <div className="resume">
+          <HeaderH3 title="Front End Proficiencies" />
+          <ul>
+            {frontEnd.map((item) => (
+              <li className="proficiency">- {item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="resume">
+          <HeaderH3 title="Back End Proficiencies" />
+          <ul>
+            {backEnd.map((item) => (
+              <li className="proficiency">- {item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="resume">
+          <HeaderH3 title="Source Control Proficiencies" />
+          <ul>
+            {sourceControl.map((item) => (
+              <li className="proficiency">- {item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
